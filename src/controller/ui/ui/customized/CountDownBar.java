125e23de17c92db934edd63bf56ce79f56739775
robert.kessler <robert.kessler@klarna.com>
2017-07-21 19:54:32 +0200

    Fixed count down bar

--- src/controller/ui/ui/customized/CountDownBar.java
+++ src/controller/ui/ui/customized/CountDownBar.java
@@ -11,34 +11,26 @@ import java.awt.geom.Arc2D;
  */
 public class CountDownBar extends JPanel {
 
-    private int dx = 50;
-    private int dy = 50;
     private double percent = 0;
 
     public CountDownBar() {
         super();
         this.repaint();
         this.setOpaque(false);
-        this.setVisible(false);
 
-        this.addComponentListener(new ComponentAdapter() {
-            public void componentResized(ComponentEvent e) {
-                // This is only called when the user releases the mouse button.
-                dx = (int) e.getComponent().getSize().getWidth();
-                dy = (int) e.getComponent().getSize().getHeight();
-            }
-        });
+
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         if (this.isVisible()) {
+
             g.setColor(new Color(255, 91, 88));
             Graphics2D g2 = (Graphics2D) g;
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-            g2.fillRect(0, 0, (int) (dx * (100 - percent) * 0.01), dy);
+            g2.fillRect(0, 0, (int) (this.getWidth() * (100 - percent) * 0.01), getHeight());
         }
     }
 
