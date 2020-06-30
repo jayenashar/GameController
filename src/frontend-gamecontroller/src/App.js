bbc6bccdd35631704b396ac5bfb25c6fffda67b1
Jayen <jayen@jayenashar.org>
2020-07-01 00:39:38 +1000

    start on information hierarchy

--- src/frontend-gamecontroller/src/App.js
+++ src/frontend-gamecontroller/src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+function GamePeriods() {
+    return (
+        <div className='game-periods'>
+            <div className='game-periods-periods'>
+                <button data-active={true}>1st Half</button>
+                <button disabled>2nd Half</button>
+                {/*<button disabled>1st Extra</button>*/}
+                {/*<button disabled>2nd Extra</button>*/}
+                <button disabled>Penalty</button>
+                <button>Referee Timeout</button>
+                <button>Drop Ball</button>
+            </div>
+            <GamePeriod/>
+        </div>
+    );
+}
+
+function GamePeriod() {
+    return (
+        <div className='game-period'>
+            <div className='game-period-states'>
+                <button disabled>Initial</button>
+                <button disabled>Ready</button>
+                <button disabled>Set</button>
+                <button data-active={true}>Playing</button>
+                <button>Finished</button>
+            </div>
+            <div className='game-period-time'>
+                10:02
+            </div>
+        </div>
+    );
+}
+
+function Teams() {
+    return (
+        <div className='teams'>
+
+        </div>
+    );
+}
+
+function ActionList() {
+    return (
+        <div className='action-list'>
+
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='game'>
+            <GamePeriods/>
+            <Teams/>
+            <ActionList/>
+        </div>
+    );
 }
 
 export default App;
